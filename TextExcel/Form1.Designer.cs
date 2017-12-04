1857cacac4cf15d532efac80199ad707c4d4a9b3
majumdarsujoy <majumdar.sujoy@gmail.com>
2017-12-04 17:23:29 +1100

    First commit

---
+++ TextExcel/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TextExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Execute = new System.Windows.Forms.Button();
+            this.btn_BrowseDestinationFile = new System.Windows.Forms.Button();
+            this.btn_BrowseSource = new System.Windows.Forms.Button();
+            this.txt_SourceFileLocation = new System.Windows.Forms.TextBox();
+            this.txt_DestinationFileLocation = new System.Windows.Forms.TextBox();
+            this.lbl_Result = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Execute
+            // 
+            this.btn_Execute.Location = new System.Drawing.Point(420, 193);
+            this.btn_Execute.Name = "btn_Execute";
+            this.btn_Execute.Size = new System.Drawing.Size(138, 23);
+            this.btn_Execute.TabIndex = 0;
+            this.btn_Execute.Text = "Execute";
+            this.btn_Execute.UseVisualStyleBackColor = true;
+            this.btn_Execute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_BrowseDestinationFile
+            // 
+            this.btn_BrowseDestinationFile.Location = new System.Drawing.Point(420, 146);
+            this.btn_BrowseDestinationFile.Name = "btn_BrowseDestinationFile";
+            this.btn_BrowseDestinationFile.Size = new System.Drawing.Size(138, 23);
+            this.btn_BrowseDestinationFile.TabIndex = 1;
+            this.btn_BrowseDestinationFile.Text = "Browse Destination File";
+            this.btn_BrowseDestinationFile.UseVisualStyleBackColor = true;
+            this.btn_BrowseDestinationFile.Click += new System.EventHandler(this.btn_BrowseDestinationFile_Click);
+            // 
+            // btn_BrowseSource
+            // 
+            this.btn_BrowseSource.Location = new System.Drawing.Point(420, 99);
+            this.btn_BrowseSource.Name = "btn_BrowseSource";
+            this.btn_BrowseSource.Size = new System.Drawing.Size(138, 23);
+            this.btn_BrowseSource.TabIndex = 2;
+            this.btn_BrowseSource.Text = "Browse Source File";
+            this.btn_BrowseSource.UseVisualStyleBackColor = true;
+            this.btn_BrowseSource.Click += new System.EventHandler(this.btn_BrowseSourceFile_Click);
+            // 
+            // txt_SourceFileLocation
+            // 
+            this.txt_SourceFileLocation.Location = new System.Drawing.Point(206, 101);
+            this.txt_SourceFileLocation.Name = "txt_SourceFileLocation";
+            this.txt_SourceFileLocation.Size = new System.Drawing.Size(188, 20);
+            this.txt_SourceFileLocation.TabIndex = 3;
+            // 
+            // txt_DestinationFileLocation
+            // 
+            this.txt_DestinationFileLocation.Location = new System.Drawing.Point(206, 146);
+            this.txt_DestinationFileLocation.Name = "txt_DestinationFileLocation";
+            this.txt_DestinationFileLocation.Size = new System.Drawing.Size(188, 20);
+            this.txt_DestinationFileLocation.TabIndex = 4;
+            // 
+            // lbl_Result
+            // 
+            this.lbl_Result.AutoSize = true;
+            this.lbl_Result.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Result.Location = new System.Drawing.Point(49, 212);
+            this.lbl_Result.Name = "lbl_Result";
+            this.lbl_Result.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Result.TabIndex = 5;
+            this.lbl_Result.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(79, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kleber Utility Tool";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(203, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 91);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Column Header\r\n-----------------\r\nAddressLine\r\nCity\r\nPostCode\r\nState\r\nDPID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(289, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 91);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Position\r\n---------\r\n10\r\n13\r\n15\r\n14\r\n16";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 368);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Result);
+            this.Controls.Add(this.txt_DestinationFileLocation);
+            this.Controls.Add(this.txt_SourceFileLocation);
+            this.Controls.Add(this.btn_BrowseSource);
+            this.Controls.Add(this.btn_BrowseDestinationFile);
+            this.Controls.Add(this.btn_Execute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Execute;
+        private System.Windows.Forms.Button btn_BrowseDestinationFile;
+        private System.Windows.Forms.Button btn_BrowseSource;
+        private System.Windows.Forms.TextBox txt_SourceFileLocation;
+        private System.Windows.Forms.TextBox txt_DestinationFileLocation;
+        private System.Windows.Forms.Label lbl_Result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TextExcel/Form1.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+using LinqToExcel;
+using LinqToExcel.Attributes;
+
+using TextExcel.KleberServicePostCode;
+
+using Excel=Microsoft.Office.Interop.Excel;
+
+
+namespace TextExcel
+{
+
+    //public sealed class OpenFileDialog : FileDialog
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        public static string destinationFile;// = txt_SourceFileLocation.Text.ToString();// @"C:\Kleber\AcquisitionFileUpload.xlsx";
+        public static string sourceFile;
+        //public static BindingList<Address> AddressList = new BindingList<Address>();
+        private static Excel.Workbook MyBook = null;
+        private static Excel.Application MyApp = null;
+        private static Excel.Worksheet MySheet = null;
+        private static int lastRow = 0;
+        public string msg_Success = "Job completed succesfully";
+        public string msg_Failure = "Job couldnt be completed . Encountered errors";
+        public string msg_InProgress = "Job in progress";
+        public static ExcelQueryFactory excel;
+
+        int counter = 1;
+        string DtResponseXml = null;
+        string FinalDtResponseXml = null;
+        string DtRequestXml = null;
+
+        public static void InitializeExcel()
+        {
+            try
+            {
+
+                //open the source file
+                excel = new ExcelQueryFactory(sourceFile)
+                {
+                    DatabaseEngine = LinqToExcel.Domain.DatabaseEngine.Ace,
+                    TrimSpaces = LinqToExcel.Query.TrimSpacesType.Both,
+                    UsePersistentConnection = true,
+                    ReadOnly = true
+                };
+
+                /// open the destination file
+                MyApp = new Excel.Application();
+                MyApp.Visible = false;
+                MyBook = MyApp.Workbooks.Open(destinationFile);
+                MySheet = (Excel.Worksheet)MyBook.Sheets[1]; // Explict cast is not required here
+                lastRow = MySheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+
+            }
+            catch (Exception ex)
+            {
+
+                MyBook.Close();
+                //MySheet.Unprotect();
+                MyApp.Quit();
+            }
+
+        }
+
+        public void useKleber( int lLimit,int uLimit)// This is the metod used for calling the kleber web service
+        {
+
+            var address = from p in excel.Worksheet<Address>(0) select p;
+
+                XmlWriterSettings XmlWriterSettings = new XmlWriterSettings();
+                XmlWriterSettings.Indent = true;
+                XmlWriterSettings.OmitXmlDeclaration = true;
+                StringBuilder XmlStringBuilder = new StringBuilder();
+                XmlWriter XmlWriter = XmlWriter.Create(XmlStringBuilder, XmlWriterSettings);
+
+                XmlWriter.WriteStartElement("DtRequests");
+
+                for (int i = lLimit; i <= uLimit; i++)
+                {
+
+                    var add = new Address();
+                    add = address.First(h => h.RequestId == i);
+                    // Create DtRequest Query XML
+                    XmlWriter.WriteStartElement("DtRequest");
+                    XmlWriter.WriteAttributeString("Method", "DataTools.Verify.Address.AuPaf.VerifyAddress");
+                    XmlWriter.WriteAttributeString("AddressLine1", add.Street1);
+                    XmlWriter.WriteAttributeString("AddressLine2", add.Street2);
+                    XmlWriter.WriteAttributeString("AddressLine3", add.Street3);
+                    XmlWriter.WriteAttributeString("AddressLine4", "");
+                    XmlWriter.WriteAttributeString("AddressLine5", "");
+                    XmlWriter.WriteAttributeString("AddressLine6", "");
+                    XmlWriter.WriteAttributeString("Locality", add.City);
+                    XmlWriter.WriteAttributeString("State", add.State);
+                    XmlWriter.WriteAttributeString("Postcode", add.Postcode);
+                    XmlWriter.WriteAttributeString("RequestId", add.RequestId.ToString());
+                    XmlWriter.WriteAttributeString("RequestKey", "RK-5C026-D8FBF-E2B0C-20FF4-EE767-D7BF8-1F1CE-EA29F");
+                    XmlWriter.WriteAttributeString("DepartmentCode", "");
+                    XmlWriter.WriteEndElement();
+
+                }
+                
+                XmlWriter.WriteEndElement();
+                XmlWriter.Close();
+                DtRequestXml = XmlStringBuilder.ToString();
+                XmlWriter.Dispose();
+
+                //---------------------------------------------------------------------------------------------
+                //Send DtRequest to Kleber Server for processing
+                DtKleberServiceClient KleberServer = new DtKleberServiceClient("BasicHttpBinding_IDtKleberService");
+                DtResponseXml = KleberServer.ProcessXmlRequest(DtRequestXml);
+                FinalDtResponseXml = FinalDtResponseXml + DtResponseXml;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //useKleber(1, 200);
+            lbl_Result.Text = msg_InProgress;
+            var excel = new ExcelQueryFactory(sourceFile)
+            {
+                DatabaseEngine = LinqToExcel.Domain.DatabaseEngine.Ace,
+                TrimSpaces = LinqToExcel.Query.TrimSpacesType.Both,
+                UsePersistentConnection = true,
+                ReadOnly = true
+            };
+            var address = from p in excel.Worksheet<Address>(0) select p;
+            int combi = 0;
+            int lowerLimit = 1;
+            int CountOfRecords = address.Count();//get count of records in excell sheet
+
+            //the records require to be divide by 50 - as Kleber can process only 50 records at a time.
+            
+            int remainder = CountOfRecords % 20;
+
+            int quotient = CountOfRecords / 20;
+
+            try
+            {
+                InitializeExcel();
+            }
+            catch (Exception ex)
+            {
+                lbl_Result.Text = msg_Failure;
+            }
+
+            if (quotient > 0) //the quotitent is more than 1 -meaning the number of cycles loop
+            {
+                
+                for (int j = 1; j <= quotient; j++)
+                {
+                    useKleber(counter, (counter + 19));
+                    counter = counter + 20;
+                }
+
+                if (remainder > 0)
+                {
+                    useKleber(((quotient * 20) + 1), ((quotient * 20) + remainder));
+
+                }
+
+            }
+            else
+            {
+                useKleber(1, remainder);
+            }
+
+            FinalDtResponseXml = "<EmbeddedByVinnies>" + FinalDtResponseXml + "</EmbeddedByVinnies>";
+
+            StringBuilder XmlResponseStringBuilder = new StringBuilder();
+            int ResultCounter = 0;
+            string responseFetchedReqId = null;
+            string responseFetchedValue = null;
+            string resultFetchedName = null;
+            string resultFetchedValue = null;
+
+            int position = 1;
+
+                    XmlReader XmlReader = XmlReader.Create(new StringReader(FinalDtResponseXml));
+            try
+            {
+                while (XmlReader.Read())
+                {
+                    lastRow += 1;
+                    if (XmlReader.IsStartElement())
+                    {
+                        switch (XmlReader.Name)
+                        {
+                            case "DtResponse":
+                                //Console.WriteLine("DT RESPONSE");
+                                if (XmlReader.HasAttributes)
+                                {
+                                    position = 1;
+                                    while (XmlReader.MoveToNextAttribute())
+                                    {
+                                        switch (XmlReader.Name)
+                                        {
+                                            case "RequestId":
+                                                responseFetchedReqId = XmlReader.Value;
+
+                                                break;
+                                        }
+                                    }
+                                    XmlReader.MoveToElement();
+                                }
+                                //Console.WriteLine(DisplayDoubleDividerString);
+                                break;
+                            case "Result":
+                                
+                                //Console.WriteLine("RESULT " + ResultCounter);
+                                position = Convert.ToInt32(responseFetchedReqId); position++;
+                                if (XmlReader.HasAttributes)
+                                {
+                                    //position = 1 ;
+                                    while (XmlReader.MoveToNextAttribute())
+                                    {
+                                        string DPIDFetched = XmlReader["DPID"].ToString(); ;
+
+                                        if (DPIDFetched != String.Empty)
+                                        { 
+
+                                            switch (XmlReader.Name)
+                                            {
+
+                                                case "AddressLine":
+
+                                                    resultFetchedName = XmlReader.Name;
+                                                    resultFetchedValue = XmlReader.Value;
+
+                                                    MySheet.Cells[position, 10] = resultFetchedValue;
+                                                    break;
+
+                                                case "City":
+
+                                                    resultFetchedName = XmlReader.Name;
+                                                    resultFetchedValue = XmlReader.Value;
+
+                                                    MySheet.Cells[position, 13] = resultFetchedValue;
+                                                    break;
+
+                                                case "Postcode":
+
+                                                    resultFetchedName = XmlReader.Name;
+                                                    resultFetchedValue = XmlReader.Value;
+
+                                                    MySheet.Cells[position, 15] = resultFetchedValue;
+                                                    break;
+
+                                                case "State":
+
+                                                    resultFetchedName = XmlReader.Name;
+                                                    resultFetchedValue = XmlReader.Value;
+
+                                                    MySheet.Cells[position, 14] = resultFetchedValue;
+                                                    break;
+
+                                                case "DPID":
+
+                                                    resultFetchedName = XmlReader.Name;
+                                                    resultFetchedValue = XmlReader.Value;
+                                                    MySheet.Cells[position, 16] = resultFetchedValue;
+                                                    ResultCounter++;
+                                                    break;
+                                            }
+                                        }
+
+
+                                    }
+                                    XmlReader.MoveToElement();
+                                }
+                                //Console.WriteLine(DisplayDividerString);
+                                break;
+
+                        }
+
+                    }
+                    MyBook.Save();
+                    ResultCounter++;
+                }
+
+                MyBook.Save();
+                string XMLReaderDump = XmlReader.ToString();
+                XmlReader.Dispose();
+
+                MyBook.Saved = true;
+                MyBook.Close();
+                //MySheet.Unprotect();
+                MyApp.Quit();
+                lbl_Result.Text = msg_Success;//"Job completed succesfully";
+            }
+            catch (Exception ex)
+            {
+                XmlReader.Dispose();
+
+                //MyBook.Saved = true;
+                MyBook.Close();
+                //MySheet.Unprotect();
+                MyApp.Quit();
+                lbl_Result.Text = msg_Failure;// "Job couldnt be completed . Encountered errors";
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_BrowseDestinationFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                txt_DestinationFileLocation.Text = file;
+                destinationFile = file;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                    lbl_Result.Text = msg_Failure;
+                }
+            }
+
+        }
+
+        private void btn_BrowseSourceFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                txt_SourceFileLocation.Text = file;
+                sourceFile = file;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                    lbl_Result.Text = msg_Failure;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+    internal class Address
+    {
+
+        [ExcelColumn("Street 1")]
+        public string Street1 { get; set; }
+
+        [ExcelColumn("Street 2")]
+        public string Street2 { get; set; }
+
+        [ExcelColumn("Street 3")]
+        public string Street3 { get; set; }
+
+        [ExcelColumn("City")]
+        public string City { get; set; }
+
+        [ExcelColumn("State")]
+        public string State { get; set; }
+
+        [ExcelColumn("Postcode")]
+        public string Postcode { get; set; }
+
+        [ExcelColumn("RequestId")]
+        public int RequestId { get; set; }
+
+    }
+}
